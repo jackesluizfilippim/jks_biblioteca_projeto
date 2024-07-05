058b4410297e1c4904a5aae6e09aff67e6087ef6
jackesluizfilippim <161051537+jackesluizfilippim@users.noreply.github.com>
2024-07-05 11:30:35 -0300

    modelo conceitual e físico, data: 05/07/2024

--- modelo 1.sql
+++ modelo 1.sql
@@ -1,25 +1,42 @@
 create database biblioteca;
+
 use biblioteca;
 
-create table livro(
-pk int,
-nome varchar(45),
-isbn varchar(255)
+
+	create table livro(
+		pk int not null auto_increment, primary key(pk),
+		nome varchar(45),
+		ISBN varchar(255)
+);
+	
+	create table emprestimo(
+		pk int not null auto_increment, primary key(pk),
+		data_emprestimo varchar(45),
+		data_devolucao varchar(45),
+		livro_fk  int not null, foreign key (livro_fk) references livro (pk),
+        aluno_fk int not null, foreign key (aluno_fk) references aluno (pk)
 );
 
-create table emprestimo(
-pk int,
-data_emprestimo varchar(45),
-data_devolucao varchar(45)
+	create table aluno(
+		pk int not null auto_increment, primary key(pk),
+		nome varchar(45),
+		cgm varchar(255)
 );
 
 
-create table aluno(
-pk int,
-nome varchar(45),
-cgm varchar(255)
-);
+select * from livro;
 
-select * from produto where pk = 2;
+insert into livro (pk, nome, ISBN) values (1, "palo a inocência do amor verdadeiro", "1002475");
 
-select pk, nome from  where pk = 2;
+insert into livro (pk, nome, ISBN) values (2, "orgulho e preconceito", "1008765");
+
+insert into livro (pk, nome, ISBN) values (3, "pequeno principe", "1008795");
+
+insert into livro (pk, nome, ISBN) values (pk, "pequeno principe", "1009865");
+
+select pk, data_devolucao from emprestimo where pk = 2;
+
+select * from aluno;
+
+update livro set ISBN = "1008765" where pk = 2;
+
